57ffac7ddca3cbac819fe03f0f648dd4570626e0
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-03-27 21:39:34 +0100

    Added custom_safeboot_restart_path property to support alternate API path to restart in safeboot mode

--- tools/safeboot.py
+++ tools/safeboot.py
@@ -4,10 +4,12 @@ import urllib.request
 
 quiet = False
 
+
 def status(msg):
     """Print status message to stderr"""
     if not quiet:
         critical(msg)
+
 
 def critical(msg):
     """Print critical message to stderr"""
@@ -15,24 +17,27 @@ def critical(msg):
     sys.stderr.write(msg)
     sys.stderr.write("\n")
 
+
 # open "/safeboot" on target to restart in SafeBoot-mode
 def safeboot(source, target, env):
     upload_protocol = env.GetProjectOption("upload_protocol")
     upload_port = env.GetProjectOption("upload_port")
-    if upload_protocol != "espota":        
+    if upload_protocol != "espota":
         critical("Wrong upload protocol (%s)" % upload_protocol)
         raise Exception("Wrong upload protocol!")
     else:
-        status("Trying to activate SafeBoot on: %s" % upload_port) 
-        req = urllib.request.Request('http://'+upload_port+'/safeboot', method='POST')
-        try: 
+        status("Trying to activate SafeBoot on: %s" % upload_port)
+        safeboot_path = env.GetProjectOption("custom_safeboot_restart_path", "/safeboot")
+        req = urllib.request.Request("http://" + upload_port + safeboot_path, method="POST")
+        try:
             urllib.request.urlopen(req)
         except urllib.error.URLError as e:
             critical(e)
             # Raise exception when SafeBoot cannot be activated
             pass
-    
+
         status("Activated SafeBoot on: %s" % upload_port)
+
 
 env.AddPreAction("upload", safeboot)
 env.AddPreAction("uploadfs", safeboot)
